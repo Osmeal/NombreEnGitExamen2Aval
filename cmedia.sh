a34f25e32282237fe5d038a1221f20b79f677a3d
Oscar Mercado <osmeal@floridauniversitaria.es>
2023-02-14 16:43:02 +0100

    Creacion del script cmedia.sh

---
+++ cmedia.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Error: Debe proporcionar el nombre de una ciudad como argumento."
+    exit 1
+fi
+
+ciudad=$1
+sum=0
+cont=0
+
+while read linea; do
+    if [[ $linea == $ciudad* ]]; then
+        suma = $(($sum + $(echo $linea | awk '{print $4}'))
+        contador = $(($cont + 1))
+    fi
+done < Consumos.txt
+
+promedio = $(echo "scale=2; $sum / $cont" | bc)
+echo "La media de consumo en $ciudad es: $promedio"
